105bc8d821c82edb468391b663a61191a4ac747b
Woyken <13166802+Woyken@users.noreply.github.com>
2025-04-02 17:12:24 +0300

    fix: :construction: Add dirty workaround to make selections and switches work for some properties
    Electrolux doesn't allow changing userselections witout providing programuuid for some reason, added dirty workaround to pull through data. Also handle partial updates from web socket

--- custom_components/electrolux_status/number.py
+++ custom_components/electrolux_status/number.py
@@ -94,7 +94,15 @@ class ElectroluxNumber(ElectroluxEntity, NumberEntity):
             value = time_minutes_to_seconds(value)
         client: OneAppApi = self.api
         if self.entity_source:
-            command = {self.entity_source: {self.entity_attr: value}}
+            if self.entity_source == "userSelections":
+                command = {
+                    self.entity_source: {
+                        "programUID": self.appliance_status["properties"]['reported']["userSelections"]['programUID'],
+                        self.entity_attr: value
+                    },
+                }
+            else:
+                command = {self.entity_source: {self.entity_attr: value}}
         else:
             command = {self.entity_attr: value}
         _LOGGER.debug("Electrolux set value %s", command)
