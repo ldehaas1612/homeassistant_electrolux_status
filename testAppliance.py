fb77a2bc06768931f5c95872062a6b317e202cd2
Luc de Haas <ldehaas1612@gmail.com>
2025-09-03 14:18:07 +0200

    Update testAppliance.py
    Working test with latest pyelectroluxocp

--- testAppliance.py
+++ testAppliance.py
@@ -6,15 +6,16 @@ import re
 # Fill in your username and password here :
 login="xxx"
 password="xxx"
+country="xx"
 
 async def main():
-    async with OneAppApi(login, password) as client:
+    async with OneAppApi(login, password, country) as client:
         appliances = await client.get_appliances_list()
         print("get_appliances_list :\n",json.dumps(appliances, indent=2))
 
-        info = await client.get_appliance_status(appliances[0].get("applianceId"))
+        info = await client.get_appliance_state(appliances[0].get("applianceId"))
         json_object = json.dumps(info, indent=2)
-        print("get_appliance_status :\n", json_object)
+        print("get_appliance_state :\n", json_object)
 
         info = await client.get_appliance_capabilities(appliances[0].get("applianceId"))
         json_object = json.dumps(info, indent=2)
